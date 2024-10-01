448b1e5787682cf489167f74c5c68e02aee68acc
X9Z0 <x9z0cofd@gmail.com>
2024-10-01 21:57:50 +0530

    providers are added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { HomePage } from "@/components/HomePage";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 export default function Home() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <ConnectionProvider endpoint={"blalala"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <HomePage />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
