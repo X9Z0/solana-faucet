74bd32d700d5bc34e2b3b133b34dc6fdcd77524c
X9Z0 <x9z0cofd@gmail.com>
2024-10-02 20:04:16 +0530

    SwitchToggle added in the main page
    it is working fine

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import {
 } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { SwitchToggle } from "@/components/SwitchToggle";
 
 function App() {
   const network = WalletAdapterNetwork.Devnet;
@@ -22,9 +23,12 @@ function App() {
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className="flex justify-center items-center h-screen">
+          <div className="flex flex-col justify-center items-center h-screen">
+            <div className="w-[1000px] p-2 flex justify-end">
+              <SwitchToggle />
+            </div>
             <div className="grid grid-col-2 grid-row-2 gap-4 p-4 border-2 border-red-50 w-[1000px]">
-              <div className="flex flex-col justify-center items-center gap-4">
+              <div className="flex flex-col border-2 border-red-500 justify-center items-center gap-4">
                 <WalletMultiButton />
                 <WalletDisconnectButton />
               </div>
