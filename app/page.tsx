5ac8918b47d9ba2aa7877bfe2277e06b9d7ee2e8
X9Z0 <x9z0cofd@gmail.com>
2024-10-14 03:13:45 +0530

    Hydration error
    removed the useMemo for the api key and added the useEffect instead

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -13,34 +13,50 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 import { SwitchToggle } from "@/components/SwitchToggle";
 import Dialer from "@/components/Dialer";
 import { apikey } from "@/key";
+import { BalanceProvider } from "@/components/context/solbalance";
+import { ShowBalance } from "@/components/ShowBalance";
+import { SendToken } from "@/components/SendToken";
+import { SignMessage } from "@/components/SignMessage";
 
 function App() {
-  //api key
-  const endpoint = useMemo(() => apikey, []);
+  const [endpoint, setEndpoint] = useState<string>("");
+
+  useEffect(() => {
+    setEndpoint(apikey); // Set endpoint only on the client
+  }, []);
+
+  if (!endpoint) return null; // Avoid rendering until endpoint is set
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className="flex flex-col justify-center items-center h-screen">
-            <div className="w-[1000px] p-2 mb-3 flex justify-end">
-              <SwitchToggle />
-            </div>
-            <div className="w-[1000px] rounded-xl border bg-card shadow p-6 flex justify-between items-center ">
-              <div className="flex flex-col rounded-3xl border bg-card shadow p-4 items-center gap-4 h-[500px] w-[350px]">
-                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                  Connect Your Wallet
-                </h1>
-                <div className="flex flex-col w-full gap-4">
-                  <WalletMultiButton />
-                  <WalletDisconnectButton />
+          <BalanceProvider>
+            <div className="flex flex-col justify-center items-center h-screen">
+              <div className="w-[1000px] p-2 mb-3 flex justify-end">
+                <SwitchToggle />
+              </div>
+              <div className="w-[1000px] rounded-xl border bg-card shadow p-6 flex justify-between items-center ">
+                <div className="flex flex-col rounded-3xl border bg-card shadow p-4 items-center gap-4 h-[500px] w-[350px]">
+                  <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Connect Your Wallet
+                  </h1>
+                  <div className="flex flex-col w-full gap-4">
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                  </div>
+                </div>
+                <div className="h-full space-y-6">
+                  <ShowBalance />
+                  <SendToken />
+                  <SignMessage />
+                </div>
+                <div>
+                  <Dialer />
                 </div>
               </div>
-              <div>
-                <Dialer />
-              </div>
             </div>
-          </div>
+          </BalanceProvider>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
