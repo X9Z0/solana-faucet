7b2cdd1c5543af018e9941407b15021ddc6730db
X9Z0 <x9z0cofd@gmail.com>
2024-10-13 22:17:31 +0530

    solanalogo

---
+++ components/solanaLogo.tsx
@@ -0,0 +1,18 @@
+export const SolanaLogo = () => {
+  return (
+    <div className="flex gap-4 justify-end items-center p-4 text-2xl font-semibold">
+      Solana
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 397.7 311.7"
+        className="h-10 w-10 text-purple-500"
+        aria-label="Solana logo"
+      >
+        <path
+          fill="currentColor"
+          d="M64.6 237.9c2.4-2.4 5.7-3.8 9.2-3.8h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1l62.7-62.7zM64.6 3.8C67.1 1.4 70.4 0 73.8 0h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1L64.6 3.8zM333.1 120.1c-2.4-2.4-5.7-3.8-9.2-3.8H6.5c-5.8 0-8.7 7-4.6 11.1l62.7 62.7c2.4 2.4 5.7 3.8 9.2 3.8h317.4c5.8 0 8.7-7 4.6-11.1l-62.7-62.7z"
+        />
+      </svg>
+    </div>
+  );
+};
