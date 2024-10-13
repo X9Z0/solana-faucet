e622e156e0e7846af591d1afef1c483b2d552b15
X9Z0 <x9z0cofd@gmail.com>
2024-10-14 03:15:30 +0530

    added the main logic

--- components/SignMessage.tsx
+++ components/SignMessage.tsx
@@ -5,28 +5,45 @@ import { Button } from "./ui/button";
 import { Pen } from "lucide-react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useToast } from "@/hooks/use-toast";
+import { ed25519 } from "@noble/curves/ed25519";
+import bs58 from "bs58";
 
 export const SignMessage = () => {
-  const {publicKey, signMessage} = useWallet();
-  const {toast} = useToast();
+  const { publicKey, signMessage } = useWallet();
+  const { toast } = useToast();
   const [message, setMessage] = useState<string>("");
-  async SignMessage = () => {
-    if(!publicKey){
+  const SignMessage = async () => {
+    if (!publicKey) {
       toast({
         variant: "destructive",
         title: "Wallet is not Connected",
-      })
+      });
       return;
     }
-    if(!signMessage){
+    if (!signMessage) {
       toast({
         variant: "destructive",
         title: "Signing message is not supported",
         description: "Your wallet does not support signing messages",
-      })
+      });
       return;
     }
-  }
+    const encodedMessage = new TextEncoder().encode(message);
+    const signature = await signMessage(encodedMessage);
+
+    if (!ed25519.verify(signature, encodedMessage, publicKey.toBytes())) {
+      toast({
+        variant: "destructive",
+        title: "Message signature Invalid",
+        description: "message was not signed",
+      });
+      return;
+    }
+    toast({
+      title: "Message Signed successfully",
+      description: `Message signature: ${bs58.encode(signature)}`,
+    });
+  };
   return (
     <div className="rounded-3xl border flex flex-col justify-center bg-card shadow p-4">
       <div className="space-y-4">
@@ -34,7 +51,12 @@ export const SignMessage = () => {
           Sign Message
         </Label>
         <div className="flex gap-2">
-          <Button variant={"outline"} size={"icon"} className="h-12 w-12">
+          <Button
+            onClick={SignMessage}
+            variant={"outline"}
+            size={"icon"}
+            className="h-12 w-12"
+          >
             <Pen className="w-6 h-6 text-[#6a3093] " />
           </Button>
           <Input
