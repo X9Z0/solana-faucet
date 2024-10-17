6ddf1c3a4724296175cc328e49a96a2864f43de2
X9Z0 <x9z0cofd@gmail.com>
2024-10-17 23:40:22 +0530

    added the await

--- components/SolBalance.tsx
+++ components/SolBalance.tsx
@@ -1,21 +1,35 @@
 import { useEffect } from "react";
 import { useBalanceContext } from "./context/solbalance";
+import { motion } from "framer-motion";
 
 export const ShowBalance = () => {
   const { sol, getBalance } = useBalanceContext();
   useEffect(() => {
-    getBalance();
+    const getsolB = async () => {
+      await getBalance();
+    };
+    getsolB();
   }, [getBalance]);
 
   return (
     <div className="rounded-3xl border bg-card shadow">
       <div>
         <div className="flex items-baseline space-x-6 p-4 justify-center">
-          <div className="text-7xl font-extrabold tracking-tight">
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+            className="text-7xl font-extrabold tracking-tight"
+          >
+            {/* <div className="text-7xl font-extrabold tracking-tight"> */}
             <span className="grad-back bg-clip-text text-transparent">
               {sol.toFixed(2)}
             </span>
-          </div>
+            {/* </div> */}
+          </motion.div>
           <div className="text-4xl font-semibold text-muted-foreground">
             SOL
           </div>
