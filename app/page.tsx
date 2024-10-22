89746367e3450e25b4e3a700e82e6fdbc5159022
X9Z0 <x9z0cofd@gmail.com>
2024-10-23 01:11:52 +0530

    removing the walletButton in another components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { SwitchToggle } from "@/components/SwitchToggle";
 import Dialer from "@/components/Dialer";
@@ -17,6 +12,7 @@ import { ShowBalance } from "@/components/ShowBalance";
 import { SendToken } from "@/components/SendToken";
 import { SignMessage } from "@/components/SignMessage";
 import { motion } from "framer-motion";
+import ConnectButton from "@/components/ConnectButtons";
 function App() {
   return (
     <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
@@ -36,15 +32,7 @@ function App() {
                   <SwitchToggle />
                 </div>
                 <div className="w-[1000px] rounded-xl border bg-card shadow p-6 flex justify-between items-center ">
-                  <div className="flex flex-col rounded-3xl border bg-card shadow p-4 items-center gap-4 h-[500px] w-[350px]">
-                    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                      Connect Your Wallet
-                    </h1>
-                    <div className="flex flex-col w-full gap-4">
-                      <WalletMultiButton />
-                      <WalletDisconnectButton />
-                    </div>
-                  </div>
+                  <ConnectButton />
                   <div className="h-full space-y-6">
                     <ShowBalance />
                     <SendToken />
