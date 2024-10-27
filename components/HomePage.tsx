18107b6c28d26fdd16dd19389897c325febd5ed8
X9Z0 <x9z0cofd@gmail.com>
2024-10-28 00:08:41 +0530

    homePage all the logic

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,15 +1,39 @@
-import { AirDrop } from "./AirDrop";
-import { ConnectWallet } from "./ConnectWallet";
-import { Description } from "./Description";
+"use client";
+import { motion } from "framer-motion";
+import { SwitchToggle } from "./SwitchToggle";
+import ConnectButton from "./ConnectButtons";
+import { ShowBalance } from "./ShowBalance";
+import { SendToken } from "./SendToken";
+import { SignMessage } from "./SignMessage";
+import Dialer from "./Dialer";
 
 export const HomePage = () => {
   return (
-    <div className="flex items-center justify-center h-screen ">
-      <div className="grid grid-col-2 grid-row-2 gap-4 p-4">
-        <ConnectWallet />
-        <AirDrop />
-        <Description />
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.3,
+        ease: "easeInOut",
+      }}
+      className="relative"
+    >
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="w-[1000px] p-2 mb-3 flex justify-end">
+          <SwitchToggle />
+        </div>
+        <div className="w-[1000px] rounded-xl border bg-card shadow p-6 flex justify-between items-center ">
+          <ConnectButton />
+          <div className="h-full space-y-6">
+            <ShowBalance />
+            <SendToken />
+            <SignMessage />
+          </div>
+          <div>
+            <Dialer />
+          </div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
